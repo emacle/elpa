9b4d1f355e215770ad29ed6918674a5c3390a9ad
qiaokun <qkbeyond@gmail.com>
2020-07-16 16:20:41 +0800

    all update

---
+++ orgtbl-aggregate-20200708.2131/orgtbl-aggregate-pkg.el
@@ -0,0 +1,5 @@
+(define-package "orgtbl-aggregate" "20200708.2131" "Create an aggregated Org table from another one" 'nil :commit "99c6bc8c9b72dc9ce886a09540ed24ec83bcc056" :keywords
+  '("org" "table" "aggregation" "filtering"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
