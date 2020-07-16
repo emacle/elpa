3066a0b618e1e2c168efd3fbacd3a1e5fe3efbf8
qiaokun <qkbeyond@gmail.com>
2020-07-16 13:33:17 +0800

    smart-input-source-20200716 perfect!

---
+++ smart-input-source-20200716.156/smart-input-source-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smart-input-source" "20200716.156" "Switch OS native input source smartly" '((names "0.5") (emacs "25") (terminal-focus-reporting "0.0")) :commit "9f8126c47273d4bd8eea6fd5fa57b338da7017d3" :keywords '("convenience") :url "https://github.com/laishulu/emacs-smart-input-source")
