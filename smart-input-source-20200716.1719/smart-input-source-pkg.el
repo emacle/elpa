40ecb2250f7243fb40bf40eb47485ee218977313
qiaokun <qkbeyond@gmail.com>
2020-07-17 08:45:41 +0800

    smart-input-source-20200716 lazyman config
    add hooks for inline region activate and deactivate

---
+++ smart-input-source-20200716.1719/smart-input-source-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smart-input-source" "20200716.1719" "Switch OS native input source smartly" '((names "0.5") (emacs "25") (terminal-focus-reporting "0.0")) :commit "d9ac0478f8373346a44424cedff362cfa6872a87" :keywords '("convenience") :url "https://github.com/laishulu/emacs-smart-input-source")
