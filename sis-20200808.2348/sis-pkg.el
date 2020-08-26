9fa1a160a58588bdf29551790f35abc920ea225c
qiaokun <qkbeyond@gmail.com>
2020-08-26 08:43:56 +0800

    fix

--- sis-20200808.2348/sis-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "sis" "20200808.2348" "Less manual switch for native or OS input source (input method)." '((emacs "25.1") (terminal-focus-reporting "0.0")) :url "https://github.com/laishulu/emacs-smart-input-source")
